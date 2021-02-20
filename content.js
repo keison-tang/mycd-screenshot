e3c2a20617b9d2feef254f12fbececc646f90501
Keison Tang <m.keison.tang@gmail.com>
2021-02-20 17:56:36 +1300

    Swap day and month around to correct order

--- content.js
+++ content.js
@@ -18,8 +18,8 @@ function generateImgFilename()
     let s = 'mycd_km_screenshot_';
 
     s += d.getFullYear() 
+        + '‐' + (d.getMonth() + 1).toString().padStart(2, '0')
         + '‐' + d.getDate().toString().padStart(2, '0')
-        + '‐' + (d.getMonth() + 1).toString().padStart(2, '0') 
         + '_'
         + d.getHours().toString().padStart(2, '0')
         + '‐' + d.getMinutes().toString().padStart(2, '0')
